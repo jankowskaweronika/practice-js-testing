399a3c077358f90659ef6b172a9ba25b9f08bf57
Weronika Jankowska <wjankowska8@gmail.com>
2024-12-14 00:53:03 +0100

    task 4

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,35 @@
 export default class User {
+  constructor(obj) {
+    const { email, password } = obj;
+    if (!this.isValidEmail(email)) {
+      throw new Error('Invalid email');
+    }
 
+    if (!this.isValidPassword(password)) {
+      throw new Error('Invalid password');
+    }
+
+    this.email = email;
+    this.password = password;
+  }
+
+  getEmail() {
+    return this.email;
+  }
+
+  getPassword() {
+    return this.password;
+  }
+
+  login() {
+    return this.email.endsWith('@devmentor.pl');
+  }
+
+  isValidEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
+  isValidPassword(password) {
+    return password.length >= 6;
+  }
 }
