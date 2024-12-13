2a18414b9e3f3dc285eb4427b373905ad5f71b53
Weronika Jankowska <wjankowska8@gmail.com>
2024-12-14 00:52:38 +0100

    task 3

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,17 @@
 export default function randomNumber(min, max) {
+  if (!isNumber(min)) {
+    throwError('min must be a number');
+  }
+  if (!isNumber(max)) {
+    throwError('max must be a number');
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+function throwError(msg) {
+  throw new Error(msg);
+}
+
+function isNumber(n) {
+  return typeof n === 'number' && !Number.isNaN(n);
 }
